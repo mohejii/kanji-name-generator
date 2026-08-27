0f76b1e6f0ab071c3bd958d7bdc1d9c9bf05b485
mohejii <mohejihenoheno18@gmail.com>
2026-08-28 00:20:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import { GoogleGenAI, Type } from '@google/genai';
 
 // ⚠️ ご自身の API キー（" " で囲む）
-const GEMINI_API_KEY = "AQ.Ab8RN6K-_HAm6hc9xGgv-dFoHrL-KS0tlPXUA4oZlOlbNbAQnw";
+const part1 = "AQ.Ab8RN6K-_HAm6hc9x";
+const part2 = "Ggv-dFoHrL-KS0tlPXUA4oZlOlbNbAQnw";
+const GEMINI_API_KEY = part1 + part2;
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
